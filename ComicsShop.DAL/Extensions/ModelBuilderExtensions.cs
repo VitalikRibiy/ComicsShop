fd567738a1203509a51cc54389d4ef2d29aeccd7
Vitalii Ribii <VitalikRibiy@gmail.com>
2019-12-20 07:28:19 +0200

    repush

---
+++ ComicsShop.DAL/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,505 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublishingHouse.DAL.Entities;
+using PublishingHouse.DAL.EntityConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PublishingHouse.DAL.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void SetConfigurations(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AuthorConfiguration());
+            modelBuilder.ApplyConfiguration(new BookAuthorConfiguration());
+            modelBuilder.ApplyConfiguration(new BookCategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new BookConfiguration());
+            modelBuilder.ApplyConfiguration(new BookOrderConfiguration());
+            modelBuilder.ApplyConfiguration(new CartConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new CommentConfiguration());
+            modelBuilder.ApplyConfiguration(new NotificationConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+        }
+
+        public static void Seed(this ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<Category>()
+                .HasData(
+                    new Category
+                    {
+                        Id = 1,
+                        Name = "Marvel"
+                    },
+                    new Category
+                    {
+                        Id = 2,
+                        Name = "DC"
+                    }
+                );
+            modelBuilder.Entity<Author>()
+                .HasData(
+                    new Author
+                    {
+                        Id = 1,
+                        Name = "Name1"
+                    },
+                    new Author
+                    {
+                        Id = 2,
+                        Name = "Name2"
+                    },
+                    new Author
+                    {
+                        Id = 3,
+                        Name = "Name3"
+                    },
+                    new Author
+                    {
+                        Id = 4,
+                        Name = "Name4"
+                    },
+                    new Author
+                    {
+                        Id = 5,
+                        Name = "Name5"
+                    },
+                    new Author
+                    {
+                        Id = 6,
+                        Name = "Name6"
+                    },
+                    new Author
+                    {
+                        Id = 7,
+                        Name = "Name7"
+                    },
+                    new Author
+                    {
+                        Id = 8,
+                        Name = "Name8"
+                    },
+                    new Author
+                    {
+                        Id = 9,
+                        Name = "Name9"
+                    },
+                    new Author
+                    {
+                        Id = 10,
+                        Name = "Name10"
+                    }
+                );
+            modelBuilder.Entity<Book>()
+                .HasData(
+                    new Book
+                    {
+                        Id = 1,
+                        Title = "Marvel1",
+                        ImageUrl = "~/images/Marvel.png",
+                        Language = "Eng",
+                        Year = 2016,
+                        Format = "70х100/16 ",
+                        Cover = "Soft",
+                        PagesAmount = 50,
+                        Description = "Some Description 1",
+                        Price = 130
+                    },
+                    new Book
+                    {
+                        Id = 2,
+                        Title = "Marvel2",
+                        ImageUrl = "~/images/Marvel.png",
+                        Language = "Ukrainian",
+                        Year = 2018,
+                        Format = "60х90/16",
+                        Cover = "Hard",
+                        PagesAmount = 40,
+                        Description = "Some Description 2",
+                        Price = 100
+                    },
+                    new Book
+                    {
+                        Id = 3,
+                        Title = "DC1",
+                        ImageUrl = "~/images/DC.png",
+                        Language = "Ukrainian",
+                        Year = 2018,
+                        Format = "60х90/16",
+                        Cover = "Hard",
+                        PagesAmount = 40,
+                        Description = "Some Description 3",
+                        Price = 105
+                    },
+                    new Book
+                    {
+                        Id = 4,
+                        Title = "DC2",
+                        ImageUrl = "~/images/DC.png",
+                        Language = "Eng",
+                        Year = 2016,
+                        Format = "70х100/16 ",
+                        Cover = "Soft",
+                        PagesAmount = 50,
+                        Description = "Some Description 4",
+                        Price = 120
+                    },
+                    new Book
+                    {
+                        Id = 5,
+                        Title = "Marvel1",
+                        ImageUrl = "~/images/Marvel.png",
+                        Language = "Eng",
+                        Year = 2016,
+                        Format = "70х100/16 ",
+                        Cover = "Soft",
+                        PagesAmount = 50,
+                        Description = "Some Description 1",
+                        Price = 130
+                    },
+                    new Book
+                    {
+                        Id = 6,
+                        Title = "Marvel2",
+                        ImageUrl = "~/images/Marvel.png",
+                        Language = "Ukrainian",
+                        Year = 2018,
+                        Format = "60х90/16",
+                        Cover = "Hard",
+                        PagesAmount = 40,
+                        Description = "Some Description 2",
+                        Price = 100
+                    },
+                    new Book
+                    {
+                        Id = 7,
+                        Title = "DC1",
+                        ImageUrl = "~/images/DC.png",
+                        Language = "Ukrainian",
+                        Year = 2018,
+                        Format = "60х90/16",
+                        Cover = "Hard",
+                        PagesAmount = 40,
+                        Description = "Some Description 3",
+                        Price = 105
+                    },
+                    new Book
+                    {
+                        Id = 8,
+                        Title = "DC2",
+                        ImageUrl = "~/images/DC.png",
+                        Language = "Eng",
+                        Year = 2016,
+                        Format = "70х100/16 ",
+                        Cover = "Soft",
+                        PagesAmount = 50,
+                        Description = "Some Description 4",
+                        Price = 120
+                    },
+                    new Book
+                    {
+                        Id = 9,
+                        Title = "Marvel1",
+                        ImageUrl = "~/images/Marvel.png",
+                        Language = "Eng",
+                        Year = 2016,
+                        Format = "70х100/16 ",
+                        Cover = "Soft",
+                        PagesAmount = 50,
+                        Description = "Some Description 1",
+                        Price = 130
+                    },
+                    new Book
+                    {
+                        Id = 10,
+                        Title = "Marvel2",
+                        ImageUrl = "~/images/Marvel.png",
+                        Language = "Ukrainian",
+                        Year = 2018,
+                        Format = "60х90/16",
+                        Cover = "Hard",
+                        PagesAmount = 40,
+                        Description = "Some Description 2",
+                        Price = 100
+                    },
+                    new Book
+                    {
+                        Id = 11,
+                        Title = "DC1",
+                        ImageUrl = "~/images/DC.png",
+                        Language = "Ukrainian",
+                        Year = 2018,
+                        Format = "60х90/16",
+                        Cover = "Hard",
+                        PagesAmount = 40,
+                        Description = "Some Description 3",
+                        Price = 105
+                    },
+                    new Book
+                    {
+                        Id = 12,
+                        Title = "DC2",
+                        ImageUrl = "~/images/DC.png",
+                        Language = "Eng",
+                        Year = 2016,
+                        Format = "70х100/16 ",
+                        Cover = "Soft",
+                        PagesAmount = 50,
+                        Description = "Some Description 4",
+                        Price = 120
+                    },
+                    new Book
+                    {
+                        Id = 13,
+                        Title = "Marvel1",
+                        ImageUrl = "~/images/Marvel.png",
+                        Language = "Eng",
+                        Year = 2016,
+                        Format = "70х100/16 ",
+                        Cover = "Soft",
+                        PagesAmount = 50,
+                        Description = "Some Description 1",
+                        Price = 130
+                    },
+                    new Book
+                    {
+                        Id = 14,
+                        Title = "Marvel2",
+                        ImageUrl = "~/images/Marvel.png",
+                        Language = "Ukrainian",
+                        Year = 2018,
+                        Format = "60х90/16",
+                        Cover = "Hard",
+                        PagesAmount = 40,
+                        Description = "Some Description 2",
+                        Price = 100
+                    },
+                    new Book
+                    {
+                        Id = 15,
+                        Title = "DC1",
+                        ImageUrl = "~/images/DC.png",
+                        Language = "Ukrainian",
+                        Year = 2018,
+                        Format = "60х90/16",
+                        Cover = "Hard",
+                        PagesAmount = 40,
+                        Description = "Some Description 3",
+                        Price = 105
+                    },
+                    new Book
+                    {
+                        Id = 16,
+                        Title = "DC2",
+                        ImageUrl = "~/images/DC.png",
+                        Language = "Eng",
+                        Year = 2016,
+                        Format = "70х100/16 ",
+                        Cover = "Soft",
+                        PagesAmount = 50,
+                        Description = "Some Description 4",
+                        Price = 120
+                    }
+                );
+            modelBuilder.Entity<BookAuthor>()
+                .HasData(
+                    new BookAuthor
+                    {
+                        Id = 1,
+                        BookId = 1,
+                        AuthorId = 1
+                    },
+                    new BookAuthor
+                    {
+                        Id = 2,
+                        BookId = 2,
+                        AuthorId = 2
+                    },
+                    new BookAuthor
+                    {
+                        Id = 3,
+                        BookId = 3,
+                        AuthorId = 3
+                    },
+                    new BookAuthor
+                    {
+                        Id = 4,
+                        BookId = 4,
+                        AuthorId = 3
+                    },
+                    new BookAuthor
+                    {
+                        Id = 5,
+                        BookId = 5,
+                        AuthorId = 4
+                    },
+                    new BookAuthor
+                    {
+                        Id = 6,
+                        BookId = 6,
+                        AuthorId = 4
+                    },
+                    new BookAuthor
+                    {
+                        Id = 7,
+                        BookId = 7,
+                        AuthorId = 5
+                    },
+                    new BookAuthor
+                    {
+                        Id = 8,
+                        BookId = 8,
+                        AuthorId = 5
+                    },
+                    new BookAuthor
+                    {
+                        Id = 9,
+                        BookId = 9,
+                        AuthorId = 6
+                    },
+                    new BookAuthor
+                    {
+                        Id = 10,
+                        BookId = 10,
+                        AuthorId = 7
+                    },
+                    new BookAuthor
+                    {
+                        Id = 11,
+                        BookId = 11,
+                        AuthorId = 8
+                    },
+                    new BookAuthor
+                    {
+                        Id = 12,
+                        BookId = 12,
+                        AuthorId = 9
+                    },
+                    new BookAuthor
+                    {
+                        Id = 13,
+                        BookId = 13,
+                        AuthorId = 10
+                    },
+                    new BookAuthor
+                    {
+                        Id = 14,
+                        BookId = 14,
+                        AuthorId = 10
+                    },
+                    new BookAuthor
+                    {
+                        Id = 15,
+                        BookId = 15,
+                        AuthorId = 10
+                    },
+                    new BookAuthor
+                    {
+                        Id = 16,
+                        BookId = 16,
+                        AuthorId = 10
+                    }
+                );
+            modelBuilder.Entity<BookCategory>()
+                .HasData(
+                    new BookCategory
+                    {
+                        Id = 1,
+                        BookId = 1,
+                        CategoryId = 1
+                    },
+                    new BookCategory
+                    {
+                        Id = 2,
+                        BookId = 2,
+                        CategoryId = 1
+                    },
+                    new BookCategory
+                    {
+                        Id = 3,
+                        BookId = 3,
+                        CategoryId = 2
+                    },
+                    new BookCategory
+                    {
+                        Id = 4,
+                        BookId = 4,
+                        CategoryId = 2
+                    },
+                    new BookCategory
+                    {
+                        Id = 5,
+                        BookId = 5,
+                        CategoryId = 1
+                    },
+                    new BookCategory
+                    {
+                        Id = 6,
+                        BookId = 6,
+                        CategoryId = 1
+                    },
+                    new BookCategory
+                    {
+                        Id = 7,
+                        BookId = 7,
+                        CategoryId = 2
+                    },
+                    new BookCategory
+                    {
+                        Id = 8,
+                        BookId = 8,
+                        CategoryId = 2
+                    },
+                    new BookCategory
+                    {
+                        Id = 9,
+                        BookId = 9,
+                        CategoryId = 1
+                    },
+                    new BookCategory
+                    {
+                        Id = 10,
+                        BookId = 10,
+                        CategoryId = 1
+                    },
+                    new BookCategory
+                    {
+                        Id = 11,
+                        BookId = 11,
+                        CategoryId = 2
+                    },
+                    new BookCategory
+                    {
+                        Id = 12,
+                        BookId = 12,
+                        CategoryId = 2
+                    },
+                    new BookCategory
+                    {
+                        Id = 13,
+                        BookId = 13,
+                        CategoryId = 1
+                    },
+                    new BookCategory
+                    {
+                        Id = 14,
+                        BookId = 14,
+                        CategoryId = 1
+                    },
+                    new BookCategory
+                    {
+                        Id = 15,
+                        BookId = 15,
+                        CategoryId = 2
+                    },
+                    new BookCategory
+                    {
+                        Id = 16,
+                        BookId = 16,
+                        CategoryId = 2
+                    }
+                );
+        }
+    }
+}
---
+++ ComicsShopMVC/Startup.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PublishingHouse.BLL;
+using PublishingHouse.BLL.Interfaces;
+using PublishingHouse.BLL.MappingProfilers;
+using PublishingHouse.BLL.Services;
+using PublishingHouse.DAL;
+using PublishingHouse.DAL.Entities;
+
+namespace PublishingHouse
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }        
+
+        public IConfiguration Configuration { get; }
+        public ILifetimeScope AutofacContainer { get; private set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<PublishingHouseContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("PublishingHouseDb")));
+
+            services.AddIdentity<Customer, IdentityRole>()
+                .AddEntityFrameworkStores<PublishingHouseContext>();
+
+            //services.AddAuthentication()
+            //    .AddGoogle(options =>
+            //    {
+            //        options.ClientId = Configuration["Authentication:Google:ClientId"];
+            //        options.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+            //    })
+            //    .AddFacebook(options =>
+            //    {
+            //        options.ClientId = Configuration["Authentication:Facebook:AppId"];
+            //        options.ClientSecret = Configuration["Authentication:Facebook:AppSecret"];
+            //    });
+
+            services.AddControllersWithViews();
+
+            services.AddAutoMapper(typeof(AuthorProfile).Assembly);
+            services.AddAutoMapper(typeof(BookProfile).Assembly);
+            services.AddAutoMapper(typeof(BookCategoryProfile).Assembly);
+            services.AddAutoMapper(typeof(BookOrderProfile).Assembly);
+            services.AddAutoMapper(typeof(BookProfile).Assembly);
+            services.AddAutoMapper(typeof(CartProfile).Assembly);
+            services.AddAutoMapper(typeof(CommentProfile).Assembly);
+            services.AddAutoMapper(typeof(NotificationProvider).Assembly);
+            services.AddAutoMapper(typeof(OrderProfile).Assembly);
+
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+
+            builder.RegisterModule<BLLDependencyModule>();
+
+            AutofacContainer = builder.Build();
+
+            return new AutofacServiceProvider(AutofacContainer);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");              
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
